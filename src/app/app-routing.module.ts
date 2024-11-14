4816e293a824a908dcecdee43ab54a2a34de12c5
Artem Markelov <artem77timoha77@gmail.com>
2024-11-14 20:22:23 +0600

    Create routes for transitions between offices with a list of employees.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeesComponent} from './components/employees/employees.component';
+import {EmployeesListComponent} from './components/employees/employees-list/employees-list.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: EmployeesComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: EmployeesListComponent,
+      },
+      {
+        path: 'office',
+        component: EmployeesListComponent,
+      },
+      {
+        path: 'office/:office',
+        component: EmployeesListComponent,
+      },
+    ],
   },
 
 ];
